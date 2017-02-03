c72ecbb1760668491c8a0908d1f4cfee23d1cb97
Fidel Hechavarria <fidelh@IVGHQ.COM>
2017-02-03 18:33:12 -0500

    Middle of process.

--- scripts/app.js
+++ scripts/app.js
@@ -8,4 +8,25 @@ app.controller("mainCtrl", function( $scope, $http ) {
         $scope.todos = response.data;
     });
 
+    $scope.newTodo = {};
+
+    $scope.addTodo = function(title) {
+        $scope.newTodo = {
+            "title" : title,
+            "completed" : false
+        }
+
+        $scope.todos.push($scope.newTodo);
+        $scope.newTodo = {};
+    };
+
+    $scope.saveTodo = function(todo) {
+        console.log("The \"" + todo.title + "\" has been saved.");
+    };
+
+    $scope.deleteTodo = function(todo, index) {
+        console.log("The \"" + todo.title + "\" has been deleted.");
+        $scope.todos.splice(index, 1);
+    };
+
 });
